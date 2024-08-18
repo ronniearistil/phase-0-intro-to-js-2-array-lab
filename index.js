85ffcd6788698fe81cee2985446538f57ffa69a4
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-08-18 11:03:09 -0700

    Completed array manipulation functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+// Initial array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions (modify the original array)
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Nondestructive functions (do not modify the original array)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
